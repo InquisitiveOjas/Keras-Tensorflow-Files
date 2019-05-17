a52a905c0e68cf5a60326ef55c04ba5c740030cb
Ojas A Ramwala <ojasramwala@gmail.com>
2019-05-17 08:37:50 +0530

    Create .pb_to_.tftrt_with_graph.py

---
+++ .pb_to_.tftrt_with_graph.py
@@ -0,0 +1,34 @@
+# Convert .pb to .rt (tensorflow_tensorrt) and also save the graph in .pb format
+
+from tensorflow.python.platform import gfile
+import tensorflow as tf
+from tensorflow.contrib import tensorrt as trt
+
+graph_filename ='Alexnet_May_7.pb'
+
+f = gfile.FastGFile(graph_filename, 'rb')
+
+# define graph def object
+frozen_graph_def = tf.GraphDef()
+
+# store frozen graph from pb file
+frozen_graph_def.ParseFromString(f.read())
+
+# Parameters:
+output_node_name = "activation_9/Sigmoid"
+workspace_size = 1 << 30
+precision = "FP32"
+batch_size = 1
+
+trt_graph = trt.create_inference_graph(
+                frozen_graph_def,
+                [output_node_name],
+                max_batch_size=batch_size,
+                max_workspace_size_bytes=workspace_size,
+                precision_mode=precision)
+
+# write modified graph def to disk
+graph_filename_converted = 'flow_RT_frozen_tensorrt.pb'
+
+with gfile.FastGFile(graph_filename_converted, 'wb') as s:
+	s.write(trt_graph.SerializeToString())
