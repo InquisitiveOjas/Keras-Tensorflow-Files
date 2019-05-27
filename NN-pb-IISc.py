603a1becf781a11d503210f996a3861a140d7658
Ojas A Ramwala <ojasramwala@gmail.com>
2019-05-27 16:24:51 +0530

    Update NN-pb-IISc.py

--- NN-pb-IISc.py
+++ NN-pb-IISc.py
@@ -117,3 +117,20 @@ def printTensors(pb_file):
 
 
 printTensors("Downloads/frozen_model_Sample_model.pb")
+
+#ANOTHER CODE TO GET NAME OF NODES
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+GRAPH_PB_PATH = 'Downloads/frozen_model_Sample_model.pb'
+with tf.Session() as sess:
+   print("load graph")
+   with gfile.FastGFile(GRAPH_PB_PATH,'rb') as f:
+       graph_def = tf.GraphDef()
+   graph_def.ParseFromString(f.read())
+   sess.graph.as_default()
+   tf.import_graph_def(graph_def, name='')
+   graph_nodes=[n for n in graph_def.node]
+   names = []
+   for t in graph_nodes:
+      names.append(t.name)
+   print(names)
